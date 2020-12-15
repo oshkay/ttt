6841bec339f6ccfbcf98c90ef66a83b6fdbf9dfc
Dmitriy Oshkay <oshkay.d@gmail.com>
2020-12-15 11:17:46 +0300

    Initial commit.

---
+++ mvc_use.php
@@ -0,0 +1,13 @@
+<?php
+spl_autoload_register(function($classname) {
+    $classname = str_replace('\\','/', $classname);
+    require_once(__DIR__ . "/${classname}.php");
+});
+
+//spl_autoload_register();
+
+use MVC\Controllers\Controller;
+echo "112";
+$obj = new Controller('users/1.html');
+echo "111";
+echo $obj->render();
